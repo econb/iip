b01c8b0fb282ff9a7cfdcb7c72393d7abdfc1e68
Eduardo <16449129+econb@users.noreply.github.com>
2023-05-08 07:02:30 -0500

    Update iip.py

--- iip.py
+++ iip.py
@@ -15,7 +15,7 @@ import altair as alt
 from sentence_transformers import SentenceTransformer, util
 import streamlit as st
 
-st.title("¡BOGOTÁ ESTÁ INNOVANDO!")
+st.title("¿CÓMO SE VIVE LA INNOVACIÓN PÚBLICA EN BOGOTÁ?")
 st.markdown("Gracias al Índice de Innovación Pública IIP 2023 de **LABCapital - Veeduría Distrital** sabemos cómo están innovando las entidades distritales de Bogotá en los últimos años.")
 
 @st.cache
