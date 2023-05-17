f539a078446dbcd27b790dcad7256782a188be95
Eduardo <16449129+econb@users.noreply.github.com>
2023-05-17 11:44:08 -0500

    Update plotly.py

--- plotly.py
+++ plotly.py
@@ -2,6 +2,6 @@ import streamlit as st
 import plotly.express as px
 
 df = px.data.election()
-fig = px.scatter_3d(df, x="Joly", y="Coderre", z="Bergeron", color="winner", size="total", hover_name="district",
+fig = px.scatter_3d(df[0:10], x="Joly", y="Coderre", z="Bergeron", color="winner", size="total", hover_name="district",
                   symbol="result", color_discrete_map = {"Joly": "blue", "Bergeron": "green", "Coderre":"red"})
 st.plotly_chart(fig)
